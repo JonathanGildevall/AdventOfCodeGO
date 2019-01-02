1591bb8dba63573614b35af16a2bbbfb736f373e
jonathangildevall <jonathan.gildevall@gmail.com>
2019-01-02 15:57:23 +0100

    Day 3 part 2

--- Day3.go
+++ Day3.go
@@ -44,3 +44,54 @@ func computeResultDay3_1() int {
 	}
 	return overlapping
 }
+
+func computeResultDay3_2() int {
+
+	var claims []string
+	var err error
+	claims, err = readLines("/Users/jonathangildevall/Documents/Programming/AdventOfCodeGO/input/day3.txt")
+	if err != nil {
+		return -1
+	}
+	fabric := make([][]string, 1000)
+	for i := 0; i < len(fabric); i++ {
+		temp := make([]string, 1000)
+		fabric[i] = temp
+	}
+	overlapped := make([]bool, len(claims))
+	for i := 0; i < len(claims); i++ {
+		indexAt := strings.Index(claims[i], "@")
+		indexComma := strings.Index(claims[i], ",")
+		indexColon := strings.Index(claims[i], ":")
+		indexX := strings.Index(claims[i], "x")
+
+		id := claims[i][1 : indexAt-1]
+		x, _ := strconv.Atoi(claims[i][indexAt+2 : indexComma])
+		y, _ := strconv.Atoi(claims[i][indexComma+1 : indexColon])
+		width, _ := strconv.Atoi(claims[i][indexColon+2 : indexX])
+		heigth, _ := strconv.Atoi(claims[i][indexX+1 : len(claims[i])])
+		for tX := x; tX < x+width; tX++ {
+			for tY := y; tY < y+heigth; tY++ {
+				if fabric[tX][tY] == "" {
+					fabric[tX][tY] = id
+				} else {
+					//When 0 is overlapped
+					if len(strings.Split(fabric[tX][tY], " ")) == 1 {
+						overlappedId, _ := strconv.Atoi(fabric[tX][tY])
+						overlapped[overlappedId-1] = true
+					}
+					intId, _ := strconv.Atoi(id)
+					overlapped[intId-1] = true
+					fabric[tX][tY] = fabric[tX][tY] + " " + id
+
+				}
+			}
+		}
+	}
+	for i := 0; i < len(overlapped); i++ {
+		if !overlapped[i] {
+			return i + 1
+		}
+	}
+	return 0
+}
--- Main.go
+++ Main.go
@@ -7,5 +7,6 @@ func main() {
 	//fmt.Print(computeResultDay1_2())
 	//fmt.Print(computeResultDay2_1())
 	//fmt.Print(computeResultDay2_2())
-	fmt.Print(computeResultDay3_1())
+	//fmt.Print(computeResultDay3_1())
+	fmt.Print(computeResultDay3_2())
 }
